7ee8034f433196755ebf8dfcdc608332f23f543d
Ronak Damani <ronakdamani0@gmail.com>
2013-04-08 20:30:43 +0530

    Modify Company Form Updated

--- BillingSystem1/BillingSystem1/CompanySetup.vb
+++ BillingSystem1/BillingSystem1/CompanySetup.vb
@@ -13,12 +13,30 @@
     End Sub
 
     Private Sub btnSave_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnSave.Click
+        If btnSave.Text = "Edit" Then
+            txtLogo.Enabled = True
+            txtAddress.Enabled = True
+            txtEmailId.Enabled = True
+            txtMobileNo.Enabled = True
+            txtWebSite.Enabled = True
+            btnSave.Text = "Save"
+            btnReset.Enabled = True
+        ElseIf btnSave.Text = "Save" Then
 
-        Dim da As BillingSystemTableAdapters.CompanySetupTableAdapter
-        da = New BillingSystemTableAdapters.CompanySetupTableAdapter
+            Dim da As BillingSystemTableAdapters.CompanySetupTableAdapter
+            da = New BillingSystemTableAdapters.CompanySetupTableAdapter
+            Dim saved As Integer = 1
+            da.UpdateCompanyDetails(txtLogo.Text, txtAddress.Text, txtEmailId.Text, txtMobileNo.Text, txtWebSite.Text)
+            MessageBox.Show("Updated Successfully", "Company Setup", MessageBoxButtons.OK, MessageBoxIcon.Information)
+            btnSave.Text = "Edit"
+            txtAddress.Enabled = False
+            txtLogo.Enabled = False
+            txtEmailId.Enabled = False
+            txtMobileNo.Enabled = False
+            txtWebSite.Enabled = False
+            btnReset.Enabled = False
+        End If
 
-        da.UpdateCompanyDetails(txtLogo.Text, txtAddress.Text, txtEmailId.Text, txtMobileNo.Text, txtWebSite.Text)
-        MessageBox.Show("Updated Successfully", "Company Setup", MessageBoxButtons.OK, MessageBoxIcon.Information)
 
     End Sub
 
@@ -30,5 +48,39 @@
         txtMobileNo.Text = ""
         txtWebSite.Text = ""
 
+    End Sub
+
+    Private Sub CompanySetup_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+
+        Dim da As BillingSystemTableAdapters.CompanySetupTableAdapter = Nothing
+        da = New BillingSystemTableAdapters.CompanySetupTableAdapter
+
+        Dim dt As DataTable = da.GetData()
+
+        For Each row As DataRow In dt.Rows
+
+            txtAddress.Enabled = False
+            txtLogo.Enabled = False
+            txtEmailId.Enabled = False
+            txtMobileNo.Enabled = False
+            txtWebSite.Enabled = False
+
+            txtAddress.Text = row.Item("C_Address")
+            txtEmailId.Text = row.Item("C_EmailId")
+            txtLogo.Text = row.Item("C_LogoPath")
+            txtMobileNo.Text = row.Item("C_PNo")
+            txtWebSite.Text = row.Item("C_WebSite")
+
+
+
+        Next
+
+        txtLogo.Enabled = False
+        txtAddress.Enabled = False
+        txtEmailId.Enabled = False
+        txtMobileNo.Enabled = False
+        txtWebSite.Enabled = False
+        btnSave.Text = "Edit"
+        btnReset.Enabled = False
     End Sub
 End Class
